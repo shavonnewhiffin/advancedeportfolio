a176e41c8e0271a4008cc6efe25c7ad23437e5a9
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-01-01 17:25:33 -0800

    toggleModal() not appearing in console

--- index.js
+++ index.js
@@ -4,13 +4,28 @@
 
 function contact(event) {
     event.preventDefault();
-  emailjs
-  .sendForm(
+const loading = document.querySelector ('.modal__overlay--loading')
+const success = document.querySelector ('.modal__overlay--success')
+loading.classList += " modal__overlay--visible";
+
+emailjs 
+.sendForm (
     'service_n4zs2wd',
     'template_oix004v',
     event.target,
-    'yMOJaQP5piIJ5gTev'
-  ).then(() => {
-    console.log('this worked1')
-  })
+    'yMOJaQP5piIJ5gTev')
+    .then(() => {
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+    }).catch (() => {
+    loading.classList.remove("modal__overlay--visible"); 
+    alert(
+        "The email service is temporarily unavailable. Please contact me directly at shavonnedwhiffin@gmail.com."
+    );  
+    })
+}
+
+function toggleModal() {
+    // toggle modal
+    console.log('toggleModal()')
 }
