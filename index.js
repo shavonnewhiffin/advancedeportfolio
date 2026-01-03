400c1d5071ec0a71d6e70a3f51b4fba87e7ecf85
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-01-02 17:32:51 -0800

    nearly finished project

--- index.js
+++ index.js
@@ -2,6 +2,37 @@
 // // service_n4zs2wd
 // template_oix004v
 
+
+let isModalOpen =false;
+let contrastToggle = false; 
+const scaleFactor = 1/20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+ 
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !==0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y *boolInt}px)`
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList.add("dark-theme")
+}
+else {
+    document.body.classList.remove("dark-theme")
+}
+}
+
+
+
+
+
 function contact(event) {
     event.preventDefault();
 const loading = document.querySelector ('.modal__overlay--loading')
@@ -16,7 +47,7 @@ emailjs
     'yMOJaQP5piIJ5gTev')
     .then(() => {
     loading.classList.remove("modal__overlay--visible");
-    success.classList += " modal__overlay--visible";
+    success.classList .add (" modal__overlay--visible");
     }).catch (() => {
     loading.classList.remove("modal__overlay--visible"); 
     alert(
@@ -24,8 +55,11 @@ emailjs
     );  
     })
 }
-
 function toggleModal() {
-    // toggle modal
-    console.log('toggleModal()')
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList.add("modal--open");
 }
